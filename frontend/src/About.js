2e5437858fa65dc6db761a300ec74578d194508b
yvoryk <163950308+yvoryk@users.noreply.github.com>
2025-03-16 14:13:08 -0400

    Completed front-end coding with Home, About, Contact pages and styles

---
+++ frontend/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h2>About BreadBasket</h2>
+      <p>BreadBasket helps you find the best grocery deals in your area.</p>
+    </section>
+  );
+}
+
+export default About;
