4e9b1a91a8fc7cf5f868dcb4998d6806140b86b2
JacksonSchreiber <45779250+JacksonSchreiber@users.noreply.github.com>
2025-03-16 18:54:49 -0700

    Added welcome <username> if user is logged in.
    Also removed login button at top if a user is currently logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react'; // Import useState
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
@@ -7,6 +7,9 @@ import Login from './Login'; // New import
 import './App.css';
 
 function App() {
+  // Track the logged in user at the App level
+  const [loggedInUser, setLoggedInUser] = useState(null); 
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -17,8 +20,19 @@ function App() {
               <li><Link to="/">Home</Link></li>
               <li><Link to="/about">About</Link></li>
               <li><Link to="/contact">Contact</Link></li>
-              <li><Link to="/login">Login</Link></li> {/* New link */}
+
+              {/* Only show "Login" link if no one is logged in */}
+              {!loggedInUser && (
+                <li><Link to="/login">Login</Link></li> 
+              )}
             </ul>
+
+            {/* Display welcome message in top right if logged in */}
+            {loggedInUser && (
+              <div style={{ position: 'absolute', top: '1rem', right: '1rem' }}>
+                Welcome, {loggedInUser}
+              </div>
+            )} 
           </nav>
         </header>
         <main>
@@ -26,7 +40,12 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<Login />} /> {/* New route */}
+
+            {/*Pass a callback to Login to set the loggedInUser */}
+            <Route 
+              path="/login" 
+              element={<Login onLoginSuccess={(user) => setLoggedInUser(user)} />} 
+            />
           </Routes>
         </main>
       </BrowserRouter>
