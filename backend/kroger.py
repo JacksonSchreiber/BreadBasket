4219217e2391ad95684a4e00f16b2c70725f3a73
jacksonschreiber <jacksonschreiber@hotmail.com>
2025-04-06 19:45:28 -0400

    Removed API client ID and secret

--- backend/kroger.py
+++ backend/kroger.py
@@ -2,13 +2,11 @@ import time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+from api_secrets import CLIENT_ID, CLIENT_SECRET
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-# Kroger API Creds:
-CLIENT_ID = "breadbasket-243261243034246a79762e693372424b5a70524e2f5771706d4548547564385547777445674f2e3847396f39366a34764d6c533555614a77492e564f3615842865638524082"
-CLIENT_SECRET = "dC-xIuXpc0n_RM8FTEoZu45hxVsVT2HL8caKjqrT"
 
 # Global variables to store the access token and its expiry time
 ACCESS_TOKEN = None
